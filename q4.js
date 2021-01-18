9bb916187b317ab14e5020573788ebd791269ad6
alonosika <alonosika@gmail.com>
2021-01-18 13:34:53 +0200

    q4 answer

---
+++ q4.js
@@ -0,0 +1,20 @@
+let num1 = Number(prompt("insert Number 1"));
+let num2 = Number(prompt("insert Number 2"));
+let num3 = Number(prompt("insert Number 3"));
+let num4 = Number(prompt("insert Number 4"));
+if (num1>num2 && num1>num3 && num1>num4)
+{
+    alert(num1);
+}
+else if  (num2>num1 && num2>num3 && num2>num4)
+{
+    alert(num2);
+}
+else if  (num3>num1 && num3>num2 && num3>num4)
+{
+    alert(num3);
+}
+else if (num4>num1 && num4>num2 && num4>num3)
+{
+    alert(num4);
+}
